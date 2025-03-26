a8484bd5f4cf66e2bcd68d051a4538bb69e2af5c
Rafael Costa  Silva <rafael.silva@mgitech.com.br>
2025-03-26 20:21:09 -0300

    First Commit

---
+++ src/main/java/com/example/crud/controllers/UsuarioController.java
@@ -0,0 +1,46 @@
+package com.example.crud.controllers;
+
+import com.example.crud.requests.RequestUsuario;
+import com.example.crud.services.UsuarioService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+        @Autowired
+        private UsuarioService usuarioService;
+
+        @GetMapping
+        public ResponseEntity getAllUsuarios() {
+            var allUsuarios = usuarioService.getAllUsuarios();
+            return ResponseEntity.ok(allUsuarios);
+        }
+
+        @PostMapping
+        public ResponseEntity registerUsuario(@RequestBody @Valid RequestUsuario data){
+            usuarioService.registerUsuario(data);
+            return ResponseEntity.ok().build();
+        }
+
+        @PutMapping
+        @Transactional
+        public ResponseEntity updateUsuario(@RequestBody @Valid RequestUsuario data){
+            var usuarioAtualizado = usuarioService.updateUsuario(data);
+            return ResponseEntity.ok(usuarioAtualizado);
+        }
+        @DeleteMapping("/{id}")
+        @Transactional
+        public ResponseEntity deleteProduct(@PathVariable UUID id){
+            usuarioService.deleteUsuario(id);
+            return ResponseEntity.noContent().build();
+        }
+
+
+}
