764b1f9f62d8a4a5f4ce11979654a7073ea28b7a
CostaCS <rafaelcostacs24@gmail.com>
2025-05-10 23:30:24 -0300

    Crud HTML de clientes

--- src/main/java/com/example/crud/controllers/ImovelController.java
+++ src/main/java/com/example/crud/controllers/ImovelController.java
@@ -3,42 +3,77 @@ package com.example.crud.controllers;
 import com.example.crud.requests.RequestImovel;
 import com.example.crud.domain.entitys.Imovel;
 import com.example.crud.services.ImovelService;
+import com.example.crud.services.ImobiliariaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
 
 import java.util.List;
 import java.util.UUID;
 
-@RestController
+@Controller
 @RequestMapping("/imovel")
 public class ImovelController {
 
     @Autowired
     private ImovelService imovelService;
 
+    @Autowired
+    private ImobiliariaService imobiliariaService;
+
     @GetMapping
+    @ResponseBody
     public ResponseEntity<List<Imovel>> getAllImoveis() {
         List<Imovel> imoveis = imovelService.getAllImoveis();
         return ResponseEntity.ok(imoveis);
     }
 
     @PostMapping
+    @ResponseBody
     public ResponseEntity<Void> registerImovel(@RequestBody @Valid RequestImovel request) {
         imovelService.registerImovel(request);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<Imovel> updateImovel(@PathVariable UUID id, @RequestBody @Valid RequestImovel request) {
         Imovel imovelAtualizado = imovelService.updateImovel(id, request);
         return ResponseEntity.ok(imovelAtualizado);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<Void> deleteImovel(@PathVariable UUID id) {
         imovelService.deleteImovel(id);
         return ResponseEntity.noContent().build();
     }
+
+    // Métodos do hmtl
+
+    @GetMapping("/pagina")
+    public String listarImoveis(Model model) {
+        model.addAttribute("imoveis", imovelService.getAllImoveis());
+        model.addAttribute("imovel", new Imovel()); // Para o formulário
+        model.addAttribute("imobiliarias", imobiliariaService.getAllImobiliarias());
+        return "imoveis";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarImovel(@ModelAttribute Imovel imovel) {
+        imovelService.salvarViaHTML(imovel);
+        return "redirect:/imovel/pagina";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarImovel(@PathVariable UUID id) {
+        imovelService.deleteImovel(id);
+        return "redirect:/imovel/pagina";
+    }
+
+
 }
